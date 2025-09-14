2cf14916eb3ac59f49f706c5073d3f8355ecacfc
KARO <karaaliissa@gmail.com>
2025-09-15 01:29:40 +0300

    Deploy Angular to Vercelaaaa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { OrdersListComponent } from './orders-list/orders-list.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { OrdersComponent } from './orders/orders.component';
 
 export const routes: Routes = [
-    { path: '', component: DashboardComponent },
+    { path: '', component: OrdersComponent },
     { path: 'orders', component: OrdersListComponent },
     // app-routing (or in your standalone bootstrap)
 { path: 'orders/:id', loadComponent: () => import('./order-detail/order-detail.component').then(m => m.OrderDetailComponent) }
